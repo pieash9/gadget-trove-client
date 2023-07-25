1fda7f77e05bca97ab70df7a64ab23f8b0d439e1
Md. pieash ahmed <81944951+pieash9@users.noreply.github.com>
2023-07-25 23:28:33 +0600

    cart

--- src/pages/Checkout/Checkout.jsx
+++ src/pages/Checkout/Checkout.jsx
@@ -1,22 +1,38 @@
 import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
 import "react-tabs/style/react-tabs.css";
+import SectionTitle from "../../components/Title/SectionTitle";
+import { FaAddressCard } from "react-icons/fa";
+import { BsCreditCardFill } from "react-icons/bs";
 
 const Checkout = () => {
   return (
-    <div>
-      <Tabs>
-        <TabList className="text-center md:w-3/4 mx-auto bg-[#F9F9F9] flex justify-around">
-          <Tab>YOUR ADDRESS</Tab>
-          <Tab>ORDER & PAYMENT</Tab>
-        </TabList>
+    <div className="mt-5">
+      <SectionTitle title={"Checkout"} />
+      <div className="mt-5">
+        <Tabs>
+          <TabList className="text-center md:w-1/2 mx-auto bg-[#F9F9F9] flex justify-around tabs text-gray-600 py-3 shadow-md hover:shadow-xl duration-500">
+            <Tab
+              className="font-medium cursor-pointer text-sky-500"
+              selectedClassName="text-sky-500"
+            >
+              <FaAddressCard className="inline mr-2" size={24}/> YOUR ADDRESS
+            </Tab>
+            <Tab
+              className="font-medium cursor-pointer"
+              selectedClassName="text-sky-500"
+            >
+            <BsCreditCardFill className="inline mr-2" size={24}/>  ORDER & PAYMENT
+            </Tab>
+          </TabList>
 
-        <TabPanel>
-          <h2>Any content 1</h2>
-        </TabPanel>
-        <TabPanel>
-          <h2>Any content 2</h2>
-        </TabPanel>
-      </Tabs>
+          <TabPanel>
+            <h2>Any content 1</h2>
+          </TabPanel>
+          <TabPanel>
+            <h2>Any content 2</h2>
+          </TabPanel>
+        </Tabs>
+      </div>
     </div>
   );
 };
